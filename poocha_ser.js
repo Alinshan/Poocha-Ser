2753549f503c0b1c574dc6c7bf7fd8cc45a52be7
ALINSHAN <86637047+CYBER-DASHAMOOLAN@users.noreply.github.com>
2021-09-12 20:02:20 +0530

    Update poocha_ser.js

--- poocha_ser.js
+++ poocha_ser.js
@@ -1,4 +1,4 @@
-/* codded by afnanplk
+/* codded by alinshan
 */
 
 const { Sequelize } = require('sequelize');
@@ -14,8 +14,8 @@ module.exports = {
  OB_NAME: process.env.Z_BOT_NAME === undefined ? 'Pinky' : process.env.Z_BOT_NAME,
  OA_NAME: process.env.Z_DEP_NAME === undefined ? 'plk' : process.env.Z_DEP_NAME,
  REMOVE2: process.env.THERI_KICK_PM === undefined ? 'false' : process.env.THERI_KICK_PM,
- PLKAFN: process.env.THERI_LIST_PM === undefined ? false : process.env.THERI_LIST_PM,
- OA_REPLY: process.env.Z_DEP_REPLY === undefined ? 'you typed plk , he is my creator' : process.env.Z_DEP_REPLY,
+ ALINAFN: process.env.THERI_LIST_PM === undefined ? false : process.env.THERI_LIST_PM,
+ OA_REPLY: process.env.Z_DEP_REPLY === undefined ? 'you typed alin , he is my creator' : process.env.Z_DEP_REPLY,
  PHONE: process.env.NUMBER === undefined ? '+918301985859' : process.env.NUMBER,   
     
 

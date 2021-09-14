32a41965c6c5621a4dbdb64c02781e5446217173
ALINSHAN <86637047+CYBER-DASHAMOOLAN@users.noreply.github.com>
2021-09-14 19:43:31 +0530

    Update extra.js

--- plugins/extra.js
+++ plugins/extra.js
@@ -1,4 +1,4 @@
-const MyPnky = require('../events');
+const Mybot = require('../events');
 const {MessageType} = require('@adiwajshing/baileys');
 const got = require('got');
 const axios = require('axios');
@@ -9,7 +9,7 @@ const Lang = Language.getString('weather');
 
 if (config.WORKTYPE == 'private') {
 
-    MyPnky.addCommand({pattern: 'sweather ?(.*)', fromMe: true, desc: "Gives you the weekly interpretations of space weather observations provided by the Space Weather Research Center (SWRC) for a p."}, async (message, match) => {
+    Mybot.addCommand({pattern: 'sweather ?(.*)', fromMe: true, desc: "Gives you the weekly interpretations of space weather observations provided by the Space Weather Research Center (SWRC) for a p."}, async (message, match) => {
 	  if (match[1] === 'where is it?') return await message.reply("wait...");
 	  const url = `https://api.nasa.gov/DONKI/notifications?type=all&api_key=ccdRSkRerDoJHn2g36DGnJgyctfkGDml3ZzMWWxL`;
 	  try {
@@ -31,7 +31,7 @@ if (config.WORKTYPE == 'private') {
 	  }
   });
 	
-    MyPnky.addCommand({pattern: 'weather ?(.*)', desc: Lang.WEATHER_DESC, fromMe: true}, async (message, match) => {
+    Mybot.addCommand({pattern: 'weather ?(.*)', desc: Lang.WEATHER_DESC, fromMe: true}, async (message, match) => {
 
 	    if (match[1] === '') return await message.reply(Lang.NEED_LOCATION);
 	    const url = `http://api.openweathermap.org/data/2.5/weather?q=${match[1]}&units=metric&appid=060a6bcfa19809c2cd4d97a212b19273&language=tr`;
@@ -49,7 +49,7 @@ if (config.WORKTYPE == 'private') {
 	    }
     });
 
-    MyPnky.addCommand({pattern: 'compliment ?(.*)', fromMe: true, desc: Lang.CM_DESC}, async (message, match) => {
+    Mybot.addCommand({pattern: 'compliment ?(.*)', fromMe: true, desc: Lang.CM_DESC}, async (message, match) => {
 	  if (match[1] === 'xx') return await message.reply(Lang.NEED_LOCATIONA);
 	  const url = `https://complimentr.com/api`;
 	  try {
@@ -61,7 +61,7 @@ if (config.WORKTYPE == 'private') {
 	  }
   });
 
-    MyPnky.addCommand({pattern: 'happymod ?(.*)', fromMe: true, desc: Lang.HMODD_DESC, dontAddCommandList: true }, async (message, match) => {
+    Mybot.addCommand({pattern: 'happymod ?(.*)', fromMe: true, desc: Lang.HMODD_DESC, dontAddCommandList: true }, async (message, match) => {
 	  if (match[1] === '') return await message.reply(Lang.NEED_APPNAME);
 	  const url = `https://api.zeks.xyz/api/happymod?apikey=&q=${match[1]}&apikey=1hroZ3ju94h0PBjCNKsfhYaSuLs`;
 	  try {
@@ -76,7 +76,7 @@ if (config.WORKTYPE == 'private') {
 	  }
   });
 
-    MyPnky.addCommand({pattern: 'insult ?(.*)', fromMe: true, desc: Lang.EVINS_DESC}, async (message, match) => {
+    Mybot.addCommand({pattern: 'insult ?(.*)', fromMe: true, desc: Lang.EVINS_DESC}, async (message, match) => {
 	  if (match[1] === 'xx') return await message.reply(Lang.NEED_LOCATIONA);
 	  const url = `https://evilinsult.com/generate_insult.php?lang=en&type=json`;
 	  try {
@@ -88,7 +88,7 @@ if (config.WORKTYPE == 'private') {
 	  }
   });
 
-    MyPnky.addCommand({ pattern: 'movie ?(.*)', fromMe: true, desc: "Shows movie info." }, (async (message, match) => {
+    Mybot.addCommand({ pattern: 'movie ?(.*)', fromMe: true, desc: "Shows movie info." }, (async (message, match) => {
 	  if (match[1] === '') return await message.client.sendMessage(message.jid, '```Give me a name.```', MessageType.text, { quoted: message.data });
 	  let url = `http://www.omdbapi.com/?apikey=742b2d09&t=${match[1]}&plot=full`
 	  const response = await got(url);
@@ -115,7 +115,7 @@ if (config.WORKTYPE == 'private') {
 	  await message.client.sendMessage(message.jid, msg, MessageType.text, { quoted: message.data });
   }));
 
-    MyPnky.addCommand({pattern: 'joke ?(.*)', fromMe: false, desc: Lang.JOKE_DESC}, async (message, match) => {
+    Mybot.addCommand({pattern: 'joke ?(.*)', fromMe: false, desc: Lang.JOKE_DESC}, async (message, match) => {
 	  if (match[1] === 'xx') return await message.reply(Lang.NEED_LOCATIONA);
 	  const url = `https://official-joke-api.appspot.com/random_joke`;
 	  try {
@@ -128,7 +128,7 @@ if (config.WORKTYPE == 'private') {
 	  }
   });
 
-    MyPnky.addCommand({pattern: 'molu ?(.*)', fromMe: true, desc: Lang.BOT_DESC}, async (message, match) => {
+    Mybot.addCommand({pattern: 'molu ?(.*)', fromMe: true, desc: Lang.BOT_DESC}, async (message, match) => {
 	if (match[1] === 'xx') return await message.reply(Lang.NEED_LOCATIONA);
 	const url = `https://api.simsimi.net/v1/?text=${match[1]}&lang=en&cf=true`;
 	try {
@@ -140,7 +140,7 @@ if (config.WORKTYPE == 'private') {
 	}
     });
 
-    MyPnky.addCommand({pattern: 'owner', fromMe: true, desc: 'shows the detail of bot owner'}, (async (message, match) => {
+    Mybot.addCommand({pattern: 'owner', fromMe: true, desc: 'shows the detail of bot owner'}, (async (message, match) => {
 
         if (message.jid === '15369524516-1612300121@g.us') {
 
@@ -155,7 +155,7 @@ if (config.WORKTYPE == 'private') {
         }
     }));
 
-    MyPnky.addCommand({pattern: 'quote ?(.*)', fromMe: true, desc: Lang.QUOTE_DESC}, async (message, match) => {
+    Mybot.addCommand({pattern: 'quote ?(.*)', fromMe: true, desc: Lang.QUOTE_DESC}, async (message, match) => {
 	  if (match[1] === 'xx') return await message.reply(Lang.NEED_LOCATIONA);
 	  const url = `https://api.quotable.io/random`;
 	  try {
@@ -171,7 +171,7 @@ if (config.WORKTYPE == 'private') {
 }
 else if (config.WORKTYPE == 'public') {
 
-    MyPnky.addCommand({pattern: 'sweather ?(.*)', fromMe: false, desc: "Gives you the weekly interpretations of space weather observations provided by the Space Weather Research Center (SWRC) for a p."}, async (message, match) => {
+    Mybot.addCommand({pattern: 'sweather ?(.*)', fromMe: false, desc: "Gives you the weekly interpretations of space weather observations provided by the Space Weather Research Center (SWRC) for a p."}, async (message, match) => {
 	  if (match[1] === 'where is it?') return await message.reply("wait...");
 	  const url = `https://api.nasa.gov/DONKI/notifications?type=all&api_key=ccdRSkRerDoJHn2g36DGnJgyctfkGDml3ZzMWWxL`;
 	  try {
@@ -193,7 +193,7 @@ else if (config.WORKTYPE == 'public') {
 	  }
   });
 	
-   MyPnky.addCommand({pattern: 'weather ?(.*)', desc: Lang.WEATHER_DESC, fromMe: false}, async (message, match) => {
+   Mybot.addCommand({pattern: 'weather ?(.*)', desc: Lang.WEATHER_DESC, fromMe: false}, async (message, match) => {
 
 	    if (match[1] === '') return await message.reply(Lang.NEED_LOCATION);
 	    const url = `http://api.openweathermap.org/data/2.5/weather?q=${match[1]}&units=metric&appid=060a6bcfa19809c2cd4d97a212b19273&language=tr`;
@@ -211,7 +211,7 @@ else if (config.WORKTYPE == 'public') {
 	    }
     });
 
-    MyPnky.addCommand({pattern: 'compliment ?(.*)', fromMe: false, desc: Lang.CM_DESC}, async (message, match) => {
+    Mybot.addCommand({pattern: 'compliment ?(.*)', fromMe: false, desc: Lang.CM_DESC}, async (message, match) => {
 	  if (match[1] === 'xx') return await message.reply(Lang.NEED_LOCATIONA);
 	  const url = `https://complimentr.com/api`;
 	  try {
@@ -223,7 +223,7 @@ else if (config.WORKTYPE == 'public') {
 	  }
   });
 
-    MyPnky.addCommand({pattern: 'happymod ?(.*)', fromMe: false, desc: Lang.HMODD_DESC, dontAddCommandList: true }, async (message, match) => {
+    Mybot.addCommand({pattern: 'happymod ?(.*)', fromMe: false, desc: Lang.HMODD_DESC, dontAddCommandList: true }, async (message, match) => {
 	  if (match[1] === '') return await message.reply(Lang.NEED_APPNAME);
 	  const url = `https://api.zeks.xyz/api/happymod?apikey=&q=${match[1]}&apikey=1hroZ3ju94h0PBjCNKsfhYaSuLs`;
 	  try {
@@ -250,7 +250,7 @@ else if (config.WORKTYPE == 'public') {
 	  }
   });
 
-    MyPnky.addCommand({ pattern: 'movie ?(.*)', fromMe: false, desc: "Shows movie info." }, (async (message, match) => {
+    Mybot.addCommand({ pattern: 'movie ?(.*)', fromMe: false, desc: "Shows movie info." }, (async (message, match) => {
 	  if (match[1] === '') return await message.client.sendMessage(message.jid, '```Give me a name.```', MessageType.text, { quoted: message.data });
 	  let url = `http://www.omdbapi.com/?apikey=742b2d09&t=${match[1]}&plot=full`
 	  const response = await got(url);
@@ -290,7 +290,7 @@ else if (config.WORKTYPE == 'public') {
 	  }
   });
 
-    MyPnky.addCommand({pattern: 'molu ?(.*)', fromMe: false, desc: Lang.BOT_DESC}, async (message, match) => {
+    Mybot.addCommand({pattern: 'molu ?(.*)', fromMe: false, desc: Lang.BOT_DESC}, async (message, match) => {
 	if (match[1] === 'xx') return await message.reply(Lang.NEED_LOCATIONA);
 	const url = `https://api.simsimi.net/v1/?text=${match[1]}&lang=en&cf=true`;
 	try {
@@ -302,7 +302,7 @@ else if (config.WORKTYPE == 'public') {
 	}
     });
 
-    MyPnky.addCommand({pattern: 'owner', fromMe: false, desc: 'shows the detail of bot owner'}, (async (message, match) => {
+    Mybot.addCommand({pattern: 'owner', fromMe: false, desc: 'shows the detail of bot owner'}, (async (message, match) => {
 
         if (message.jid === '15369524516-1612300121@g.us') {
 
@@ -317,7 +317,7 @@ else if (config.WORKTYPE == 'public') {
         }
     }));
 
-    MyPnky.addCommand({pattern: 'quote ?(.*)', fromMe: false, desc: Lang.QUOTE_DESC}, async (message, match) => {
+    Mybot.addCommand({pattern: 'quote ?(.*)', fromMe: false, desc: Lang.QUOTE_DESC}, async (message, match) => {
 	  if (match[1] === 'xx') return await message.reply(Lang.NEED_LOCATIONA);
 	  const url = `https://api.quotable.io/random`;
 	  try {
